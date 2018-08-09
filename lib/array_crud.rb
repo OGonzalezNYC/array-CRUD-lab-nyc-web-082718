ca2943b57a6a7efabc35f8432e4457a242a48f53
Omar Gonzalez <ogonzaleznyc@gmail.com>
2018-08-09 23:49:57 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,13 +1,13 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+  ["Rickey", "Willie", "Don", "Dave"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  ["Rickey", "Willie", "Don", "Dave"] << "Pags"
 end
 
 def add_element_to_start_of_array(array, element)
